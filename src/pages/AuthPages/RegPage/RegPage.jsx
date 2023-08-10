13e9c18cedb2b6f2b172d6c0e9163387338a6581
Serhii Markov <goo.komalel@gmail.com>
2023-08-10 18:47:44 +0300

    Update RegPage.jsx

--- src/pages/AuthPages/RegPage/RegPage.jsx
+++ src/pages/AuthPages/RegPage/RegPage.jsx
@@ -1,21 +1,44 @@
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { signUp } from "../redux/auth-operations";
 
 // import { RegPageStyled } from "./regPage.styled";
 import AuthBox from "../AuthBox";
+import Loader from "components/shares/Loader/Loader";
 
 
 const RegPage = () => {
+    const { loading, error } = useSelector(state => state.auth);
     const dispatch = useDispatch();
+
     const handleSignup = (data) => {
         dispatch(signUp(data));
-    }
+    };
+
+    const errStroke = (error) => {
+        if (!error) return;
+        
+        switch (error.status) {
+            case 400:
+                return <p style={{ color: "#F43F5E", marginTop:"0px" }}>{`Sorry! ${error.status}: Invalid request body`}</p>
+            case 409:
+                return <p style={{ color: "#F43F5E", marginTop:"0px" }}>{`Sorry! ${error.status}: Email in use`}</p>
+            case 500:
+                return <p style={{ color: "#F43F5E", marginTop:"0px" }}>{`Sorry! ${error.status}: Server error`}</p>
+
+            default:
+                break;
+        }
+    };
 
     return (
-        <AuthBox forWhat="Registration" handleSignup={handleSignup}>
-            <p className="auth_bottom_text">Already have an account?<Link to="/login"> Login</Link></p>
-        </AuthBox>
+        <>
+            <AuthBox forWhat="Registration" handleSignup={handleSignup}>
+                {errStroke(error)}
+                {loading && <Loader />}
+                <p className="auth_bottom_text">Already have an account?<Link to="/login"> Login</Link></p>
+            </AuthBox>
+        </>
     )
 };
 
